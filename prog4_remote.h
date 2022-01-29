b9b6d984b6368bb91d29518637f6241089e0b6ff
Rajeev Sobti <sobtirajeev@gmail.com>
2022-01-29 19:21:37 +0530

    Create prog4_remote.h

---
+++ prog4_remote.h
@@ -0,0 +1,2 @@
+/* New header file added on remote only.
+Will check on Local Repo in Mac (which is clone of this repo) as well as on Desktop (D:/Myapp_G) which was pushed to create this remote repo. (sobtirajeev/myapp_sample)  
